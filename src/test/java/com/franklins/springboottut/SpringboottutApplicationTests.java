9fa99ae318183681cbd6c81dab86d9246ab49b0b
Franklin Moon <franklin.m.moon@gmail.com>
2021-05-04 12:12:01 +1000

    Initialized project

---
+++ src/test/java/com/franklins/springboottut/SpringboottutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franklins.springboottut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
